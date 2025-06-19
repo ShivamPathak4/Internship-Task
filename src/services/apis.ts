40d33105ef38dc986f9f0e067040f7036ba0ed81
ShivamPathak4 <141670746+ShivamPathak4@users.noreply.github.com>
2025-06-19 12:20:01 +0530

    Update apis.ts

--- src/services/apis.ts
+++ src/services/apis.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "https://edtechbackend-driy.onrender.com/api/v1";
+const BASE_URL = "https://edtechbackend-ww3kjkk5.b4a.run/api/v1";
 export const endpoints = {
   SENDOTP_API: BASE_URL + "/auth/sendotp",
   SIGNUP_API: BASE_URL + "/auth/signup",
